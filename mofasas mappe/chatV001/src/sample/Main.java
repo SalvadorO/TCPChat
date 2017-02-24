d75616289b992a2aca7428da25526d86b5bc8022
mofasa1 <tafe_5@hotmail.com>
2017-02-24 14:35:02 +0100

    Add files via upload

--- mofasas mappe/chatV001/src/sample/Main.java
+++ mofasas mappe/chatV001/src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -21,8 +22,9 @@ public class Main extends Application {
             setPrimaryStage(primaryStage);
 
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Zkype ™");
         primaryStage.setScene(new Scene(root, 380, 300));
+        primaryStage.getIcons().add(new Image("file:imgs/icon2.png"));
         primaryStage.show();
     }
 
