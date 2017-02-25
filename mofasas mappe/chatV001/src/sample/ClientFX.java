1a36a0814e46ea39c4b1800190fd67b8f85cc41e
mofasa1 <tafe_5@hotmail.com>
2017-02-25 17:55:27 +0100

    Add files via upload

--- mofasas mappe/chatV001/src/sample/ClientFX.java
+++ mofasas mappe/chatV001/src/sample/ClientFX.java
@@ -1,9 +1,4 @@
 package sample;
-
-
-/**
- * Created by Mustafe on 07.02.2017.
- */
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -18,6 +13,12 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
+/**
+ * This class serves client functionality, which consist of sending and receiving a message, changing status, receiving messages.
+ * There is also a chatlog variable that saves all conversations in a Hash Map, which enables the functionality of talking to many clients at the same time
+ * on the same text area.
+ */
+
 public class ClientFX  extends Service<Void> {
     private Socket cliSocket;
     private PrintStream outStream;
@@ -29,7 +30,7 @@ public class ClientFX  extends Service<Void> {
     final ObservableList<String> observableOffline = FXCollections.observableArrayList();
     final ObservableList<String> observableBusy = FXCollections.observableArrayList();
 
-    ObservableMap<String,String> chatHistory = FXCollections.observableMap(new HashMap<>());
+    ObservableMap<String,String> chatLog = FXCollections.observableMap(new HashMap<>());
     boolean isClientOnline,loggedOn = false,isCreatingUser = false;
     private MenuItem busyButton,onlineButton;
     String hostName;
@@ -41,7 +42,18 @@ public class ClientFX  extends Service<Void> {
     Button sendButton;
 
 
-
+    /**
+     *  Constructs a new instance of this class and assigns parameter values to this class values.
+     *  The client will try to connect to the server, if not, an IOException/UnknownHostException will be trowed and a alert box will appear.
+     * @param ipAdress sets value to the ip address
+     * @param port sets value to the port number
+     * @param textField gets an javafx instance of a textfield from the controller
+     * @param textArea gets an javafx instance of a textArea from the controller
+     * @param startText gets an javafx instance of a textArea from the controller
+     * @param busyButton gets an javafx instance of a button from the controller
+     * @param onlineButton gets an javafx instance of a button from the controller
+     * @param sendButton gets an javafx instance of a button from the controller
+     */
     public ClientFX(String ipAdress, int port, TextField textField, TextArea textArea, Text startText,
                     MenuItem busyButton, MenuItem onlineButton, Button sendButton) {
         this.hostName = ipAdress;
@@ -53,9 +65,10 @@ public class ClientFX  extends Service<Void> {
         this.onlineButton = onlineButton;
         this.sendButton = sendButton;
 
-        System.out.println("Client is up!");
 
         try {
+
+            // initializes client socket, output stream and input stream.
             cliSocket = new Socket(hostName, portNumber);
             outStream = new PrintStream(cliSocket.getOutputStream());
             inStream = new BufferedReader(new InputStreamReader(cliSocket.getInputStream()));
@@ -68,32 +81,33 @@ public class ClientFX  extends Service<Void> {
             alertDialogs("Connection Error","Can not connect to server.");
         }
 
-
-
+        // starts this thread
         this.start();
 
 
     }
 
-    public Socket getCliSocket() {
-        return cliSocket;
-    }
-
-
-
+    /**
+     * creates a task that this class will run on a separate thread. This will happend once the socket has connected to a server
+     * @return returns the task this server is assigned to do.
+     */
     @Override
     protected  Task<Void> createTask() {
 
         Task<Void> task = new Task<Void>() {
             protected Void call() throws InterruptedException {
+
                 String inLine;
-                System.out.println("call");
                 sendButton.setDefaultButton(true);
+
                 try {
 
+                    // while the client has not signed in
                     while (!loggedOn){
 
                         if (!username.isEmpty() && !password.isEmpty()) {
+
+                            // if creating a new user
                             if (isCreatingUser){
                                 if (!username.matches("(\\w+)") || !password.matches("(\\w+)")) {
                                     alertDialogs("Create User", "Username and password must contain only digts or letters.");
@@ -106,6 +120,7 @@ public class ClientFX  extends Service<Void> {
                             if (username.matches("(\\w+)") || password.matches("(\\w+)")) outStream.println(username + ":" + password);
                         }
 
+                        // when a user is signed in.
                         while ((inLine = inStream.readLine()) != null) {
 
                             if (inLine.equals("[LogInApproved*OK]")){
@@ -128,6 +143,7 @@ public class ClientFX  extends Service<Void> {
 
                     }
 
+                    // when the user wants to change status to ONLINE
                     onlineButton.setOnAction(event -> {
                         outStream.println("[SetClientOnline*OK]");
                         textArea.setVisible(true);
@@ -136,6 +152,9 @@ public class ClientFX  extends Service<Void> {
                        startText.setText("");
                         startText.setVisible(false);
                     });
+
+                    // when the user wants to change status to BUSY
+
                     busyButton.setOnAction(event1 ->{
                         outStream.println("[SetClientBusy*OK]");
                         textArea.setVisible(false);
@@ -148,27 +167,29 @@ public class ClientFX  extends Service<Void> {
                     });
 
 
+                    // when the user wants to send a message
                     sendButton.setOnAction(event1 -> {
                         outStream.println("[SendingAMessage*OK]");
                         outStream.println(textField.getText());
-                        if (chatHistory.get(username2).equals(""))textArea.setText("You: " + textField.getText());
+                        if (chatLog.get(username2).equals(""))textArea.setText("You: " + textField.getText());
                         else textArea.setText(textArea.getText() + "\n"  + "You: " + textField.getText());
 
-                        chatHistory.put(username2,textArea.getText());
+                        chatLog.put(username2,textArea.getText());
 
 
                         textField.clear();
                     });
 
+                    // while receiving messages from server
                     while ((inLine = inStream.readLine()) != null) {
 
+                        //when receiving lists from the server
                         if (inLine.equals("[SendingListOfUsers*OK]")) {
                             onlineUsers.clear();
                             offlineUsers.clear();
                             busyUsers.clear();
 
                             recieveLists();
-
 
                             Platform.runLater(() ->{
 
@@ -190,23 +211,28 @@ public class ClientFX  extends Service<Void> {
 
 
                                 areUsersOnline(observableOnline.isEmpty() || (sender != null && !observableOnline.contains(sender))
-                                        || (!observableOnline.contains(username2) && !chatHistory.get(username2).equals("") ));
+                                        || (!observableOnline.contains(username2) && !chatLog.get(username2).equals("") ));
 
                             });
 
 
                         }  else {
+
+                            // when the client receives a message from another client.
                             sender = inLine;
                             String message = inStream.readLine();
                             StringBuilder chat = new StringBuilder();
 
 
 
-                            if (chatHistory.get(sender) == null || chatHistory.get(sender).equals("")) chat.append(sender + ": " + message);
-                            else chat.append(chatHistory.get(sender)+ "\n" + sender + ": " + message);
+                            // sets the received text into a hash map (chathistory). Enabling the functionality of multiple conversation
+                            // on one text area, (Many-to-many chat).
 
-                            chatHistory.put(sender,chat.toString());
-                            textArea.setText(chatHistory.get(sender));
+                            if (chatLog.get(sender) == null || chatLog.get(sender).equals("")) chat.append(sender + ": " + message);
+                            else chat.append(chatLog.get(sender)+ "\n" + sender + ": " + message);
+
+                            chatLog.put(sender,chat.toString());
+                            textArea.setText(chatLog.get(sender));
                             if (!sender.equals(username2)){
                                 setConnectTo(sender);
 
@@ -230,24 +256,43 @@ public class ClientFX  extends Service<Void> {
     }
 
 
+    /**
+     * change the state of a client
+     * @param clientOnline boolean value that changes if a client becomes online/offline
+     */
     public void setClientOnline(boolean clientOnline) {
         isClientOnline = clientOnline;
     }
 
-
+    /**
+     * set method for changing the password variable for this class
+     * @param password gets a string as password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
+    /**
+     * set method for changing the username variable for this class
+     * @param username gets a string as username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * get-method to see if this client instance (this class) is online/offline
+     * @return boolean value
+     */
     public boolean isLoggedOn() {
         return loggedOn;
     }
 
-    public void areUsersOnline(boolean check){
+    /**
+     * This method checks if there are any users online, and change the GUI components
+     * It also changes if the current client is set as busy.
+     * @param check boolean value that is used to see if the online list is empty.
+     */
+    private void areUsersOnline(boolean check){
 
         if (check){
 
@@ -279,6 +324,12 @@ public class ClientFX  extends Service<Void> {
 
     }
 
+    /**
+     * This method sets value to the variable "username2", which is the username that was clicked on, (online user list).
+     * The method takes the username that was clicked on, and sends a request for a new chat with this username.
+     * Finally, text areas and fields changes when the clients opens up a new chat with username2.
+     * @param username2 the username that the client clicked on and wants to talk to.
+     */
     public void setConnectTo(String username2) {
 
         outStream.println("[RequestingChat*OK]");
@@ -289,26 +340,28 @@ public class ClientFX  extends Service<Void> {
         sendButton.setDisable(false);
 
 
-
+        // setting a delay between the request message and username message
         try {
             Thread.sleep(200);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        // sends a message of which user this client wants to talk to.
         outStream.println(username2);
         this.username2 = username2;
 
-        textArea.setText(chatHistory.get(username2));
+        textArea.setText(chatLog.get(username2));
 
 
 
 
     }
 
-    public ObservableList<String> getObservableOnline() {
-        return observableOnline;
-    }
-
+    /**
+     * If the server sends lists of users, then this method will receive and add all usernames in a observable arraylist.
+     * @throws IOException if the socket for inputstream is not connected
+     */
     private void recieveLists() throws IOException{
 
 
@@ -320,7 +373,7 @@ public class ClientFX  extends Service<Void> {
             while ((listOfOnline = inStream.readLine()) != null && !listOfOnline.equals("[SendingOfflineList*OK]")) {
                 onlineUsers.add(listOfOnline);
 
-                if (!chatHistory.containsKey(username2))chatHistory.put(username2,"");
+                if (!chatLog.containsKey(username2)) chatLog.put(username2,"");
 
 
             }
@@ -347,11 +400,17 @@ public class ClientFX  extends Service<Void> {
 
         }
     }
-
-    private void alertDialogs(String headerText, String contentText){
+    /**
+     * When the client revives an error message from server, this method will be called.
+     * It will create an alert popup that describes the errors.
+     * @param headerText sets text on the header area.
+     * @param contentText sets text on the content area.
+     */
+    protected void alertDialogs(String headerText, String contentText){
         Platform.runLater(() -> {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("ERROR");
+            if (alert.getAlertType() == Alert.AlertType.ERROR) alert.setTitle("ERROR");
             alert.setHeaderText(headerText);
             alert.setContentText(contentText);
 
@@ -359,15 +418,47 @@ public class ClientFX  extends Service<Void> {
         });
     }
 
+    /**
+     * Observablelist that consists of usernames that are currently busy.
+     * @return the list of busy user names.
+     */
     public ObservableList<String> getObservableOffline() {
         return observableOffline;
     }
 
+    /**
+     * Observablelist that consists of usernames that are currently busy.
+     * @return the list of busy user names.
+     */
     public ObservableList<String> getObservableBusy() {
         return observableBusy;
     }
 
+    /**
+     * Observablelist that consists of usernames that are currently online.
+     * @return the list of online user names.
+     */
+    public ObservableList<String> getObservableOnline() {
+        return observableOnline;
+    }
+
+    /**
+     * sets a boolean value true if the client wants to create a new user, by clicking on "Create User".
+     * @param creatingUser sets boolean value to the parameter value.
+     */
     public void setCreatingUser(boolean creatingUser) {
         isCreatingUser = creatingUser;
+    }
+
+    /**
+     * Creating an accsess for the Controller class to get the client socket
+     * @return the socket of this client that is connected to a server.
+     */
+    public Socket getCliSocket() {
+        return cliSocket;
+    }
+
+    public BufferedReader getInStream() {
+        return inStream;
     }
 }
--- mofasas mappe/chatV001/src/sample/Controller.java
+++ mofasas mappe/chatV001/src/sample/Controller.java
@@ -5,7 +5,6 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
@@ -13,35 +12,37 @@ import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
+/**
+ * This class is responsible for controlling the flow of the application.  It sends instances of GUI components to ClientFX, port number
+ * and IP Address. This class takes action corresponding to user activities, such as changing GUI visuals and setting variables for ClientFX
+ */
+
 public class Controller implements Initializable{
 
     ClientFX client;
     private String serverIp = "localhost";
     private int port = 5555;
 
-    @FXML
-    public TextField txtField,username;
+    @FXML public TextField txtField,username;
 
-    @FXML
-    public PasswordField passwd;
+    @FXML public PasswordField passwd;
 
-    @FXML
-    public TextArea txtArea,clientInfo;
+    @FXML public TextArea txtArea,clientInfo;
 
-    @FXML
-    Button loginbutton, buttonCreateUser,editServerButton, sendButton;
+    @FXML Button loginbutton, buttonCreateUser,editServerButton, sendButton;
 
     @FXML  ListView<String> onlinelist,offlinelist,busylist;
 
-    @FXML
-    Text startText,busyText,onlineText,offlineText,passwordText,usernameText,welcomeText;
+    @FXML Text startText,busyText,onlineText,offlineText,passwordText,usernameText,welcomeText;
 
     @FXML SplitMenuButton statusButton;
 
     @FXML MenuItem busyButton,onlineButton;
 
 
-
+    /**
+     * Start the client by creating a new instance of ClientFX class and sends serverIP, port and GUI components.
+     */
     public void startClient() {
 
 
@@ -50,62 +51,27 @@ public class Controller implements Initializable{
 
     }
 
+    /**
+     * When the client gets an approval from the server, this method will run and change/toggle the javafx components.
+     * @param isSignedIn value that tells if client is allowed to sign in.
+     */
     private void whenSignedIn(boolean isSignedIn){
         if (isSignedIn) {
 
-            Main.getPrimaryStage().setMinWidth(830);
-            Main.getPrimaryStage().setMinHeight(600);
-            Main.getPrimaryStage().centerOnScreen();
+            toggleClientScene();
 
-            username.setVisible(false);
-            passwd.setVisible(false);
-            username.setDisable(true);
-            passwd.setDisable(true);
-            loginbutton.setDisable(true);
-            loginbutton.setVisible(false);
-            editServerButton.setDisable(true);
-            editServerButton.setVisible(false);
-            buttonCreateUser.setDisable(true);
-            buttonCreateUser.setVisible(false);
-
-            txtField.setVisible(true);
-            sendButton.setVisible(true);
-
-            onlineText.setVisible(true);
-            offlineText.setVisible(true);
-            busyText.setVisible(true);
-            startText.setVisible(true);
-            usernameText.setVisible(false);
-            passwordText.setVisible(false);
-            welcomeText.setText("Welcome, " + client.username + "!");
-            welcomeText.setVisible(true);
-
-            clientInfo.setDisable(false);
-            clientInfo.setText("IP Address: " + client.getCliSocket().getInetAddress() + "\n"
-                + "Port Number: " + client.getCliSocket().getPort());
-            clientInfo.setVisible(true);
-            clientInfo.setEditable(false);
-
-
-
-            statusButton.setDisable(false);
-            statusButton.setVisible(true);
-            onlinelist.setDisable(false);
-            onlinelist.setVisible(true);
-            offlinelist.setVisible(true);
-            busylist.setVisible(true);
-
-
+            // sets arraylists into the listviews
             onlinelist.setItems(client.getObservableOnline());
             offlinelist.setItems(client.getObservableOffline());
             busylist.setItems(client.getObservableBusy());
 
-
+            // makes sure that it is only able to click on one item at the time
             onlinelist.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
             offlinelist.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
             busylist.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
 
+            // listener to the onlinelist when someone clicks on a username.
             onlinelist.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
                 public void changed(ObservableValue<? extends String> observableval, String oldval, String newval) {
 
@@ -114,6 +80,8 @@ public class Controller implements Initializable{
 
 
 
+                    // if the username is not null, ServerFX class will recieve a username. This string will be used when clients wants to create
+                    // a new conversation.
                     if (connectToThisUser != null && !connectToThisUser.equals("null") && !connectToThisUser.equals("")) {
                         client.setConnectTo(connectToThisUser);
                     }
@@ -122,27 +90,35 @@ public class Controller implements Initializable{
         }
     }
     /**
-     *
+     * This method is connected to the login button, and will run when a user clicks on it. first it sends the password/username inputs
+     * to ServerFX and then waits until it gets a response from the server.
      */
     @FXML public void loginserver(){
 
-        startClient();
-        client.setPassword(passwd.getText());
-        client.setUsername(username.getText());
-        passwd.clear();
-        username.clear();
+        if (!passwd.getText().isEmpty() || !username.getText().isEmpty()){
+            startClient();
+            client.setPassword(passwd.getText());
+            client.setUsername(username.getText());
+            passwd.clear();
+            username.clear();
 
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            whenSignedIn(client.isLoggedOn());
+
         }
-
-        whenSignedIn(client.isLoggedOn());
 
     }
 
-    @FXML public void editServer(){
+    /**
+     * This methoed will create a dialogbox where the user can change the server IP address. It has "localhost" as default, which means
+     * that the server and client runs on the same machine/host.
+     */
+    @FXML public void editServerIP(){
         TextInputDialog dialog = new TextInputDialog(serverIp);
         dialog.setHeaderText("Server configurations");
         dialog.setContentText("Please enter server IP address");
@@ -151,6 +127,10 @@ public class Controller implements Initializable{
         result.ifPresent(name -> serverIp = name);
     }
 
+    /**
+     *  This method is similar to the loginserver() method however the difference is that it will send a true statement to client class
+     *  before waiting for server responds.
+     */
     @FXML public void createNewUser(){
 
         startClient();
@@ -173,10 +153,44 @@ public class Controller implements Initializable{
 
     }
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
 
+        toggleLoginScene();
+
+
+        // This makes sure that the socket that belongs to the client, is closed right before signing off (exiting the window).
+        Main.getPrimaryStage().setOnCloseRequest(event -> {
+
+            // Create a  logoff popup when clicked on exit.
+            try {
+                if (client != null && client.getCliSocket() != null) {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setHeaderText("Signed Off");
+                    alert.setContentText("You are now signed off");
+
+                    alert.showAndWait();
+
+                        // close client socket.
+                        client.getCliSocket().close();
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+
+
+    }
+
+    /**
+     *  Toggle GUI components by setting visible or disable as long as the user has not logged in.
+     */
+    private void toggleLoginScene(){
 
         txtArea.setDisable(true);
         txtArea.setVisible(false);
@@ -202,22 +216,54 @@ public class Controller implements Initializable{
         offlinelist.setVisible(false);
         busylist.setDisable(true);
         busylist.setVisible(false);
+    }
+
+    /**
+     *  toggle GUI components by setting visible/disable when the user is signed in.
+     */
+    private void toggleClientScene(){
+        Main.getPrimaryStage().setMinWidth(830);
+        Main.getPrimaryStage().setMinHeight(600);
+        Main.getPrimaryStage().centerOnScreen();
+
+        username.setVisible(false);
+        passwd.setVisible(false);
+        username.setDisable(true);
+        passwd.setDisable(true);
+        loginbutton.setDisable(true);
+        loginbutton.setVisible(false);
+        editServerButton.setDisable(true);
+        editServerButton.setVisible(false);
+        buttonCreateUser.setDisable(true);
+        buttonCreateUser.setVisible(false);
+        txtArea.setEditable(false);
+
+        txtField.setVisible(true);
+        sendButton.setVisible(true);
+
+        onlineText.setVisible(true);
+        offlineText.setVisible(true);
+        busyText.setVisible(true);
+        startText.setVisible(true);
+        usernameText.setVisible(false);
+        passwordText.setVisible(false);
+        welcomeText.setText("Welcome, " + client.username + "!");
+        welcomeText.setVisible(true);
+
+        clientInfo.setDisable(false);
+        clientInfo.setText("IP Address: " + client.getCliSocket().getInetAddress() + "\n"
+                + "Port Number: " + client.getCliSocket().getPort());
+        clientInfo.setVisible(true);
+        clientInfo.setEditable(false);
 
 
 
-
-
-        Main.getPrimaryStage().setOnCloseRequest(event -> {
-            try {
-
-                if (client != null && client.getCliSocket() != null) client.getCliSocket().close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-
-
+        statusButton.setDisable(false);
+        statusButton.setVisible(true);
+        onlinelist.setDisable(false);
+        onlinelist.setVisible(true);
+        offlinelist.setVisible(true);
+        busylist.setVisible(true);
     }
 
 
